6ea742ff93792c230cb00f2441093cf0efff3ef8
teenspirittt <vshabunin25@gmail.com>
2023-06-20 14:39:52 +0700

    working namenode

--- namenode/run.sh
+++ namenode/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cp /opt/hadoop-$HADOOP_VERSION/conf/core-site.xml /opt/hadoop-$HADOOP_VERSION/etc/hadoop/core-site.xml
-cp /opt/hadoop-$HADOOP_VERSION/conf/hdfs-site.xml /opt/hadoop-$HADOOP_VERSION/etc/hadoop/hdfs-site.xml
-
-/opt/hadoop-$HADOOP_VERSION/bin/hdfs namenode -format
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format 
 
 /opt/hadoop-$HADOOP_VERSION/sbin/hadoop-daemon.sh start namenode
